b5f1f5285e5961b81d3f8b8d4a0757dc3287fcf7
BuildTools <unconfigured@null.spigotmc.org>
2021-05-16 10:33:18 -0500

    Bug fixes, instructions added to README.md

--- Call for Help/ViewController.swift
+++ Call for Help/ViewController.swift
@@ -10,7 +10,10 @@ import AVFoundation
 import Speech
 import CoreData
 
+
+
 class ViewController: UIViewController, SFSpeechRecognizerDelegate {
+    
     //Define some variables
     private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))!
     
@@ -20,28 +23,43 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
     
     private let audioEngine = AVAudioEngine()
     
+    var called = false
     
-    
-    @IBOutlet weak var numberField: UITextField!
+    @IBOutlet weak public var numberField: UITextField!
     //Save the phone number when it is changed
     @IBAction func valueChanged(_ sender: Any) {
-        guard let appDelegate =
-            UIApplication.shared.delegate as? AppDelegate else {
-            return
-          }
-        let managedContext =
-            appDelegate.persistentContainer.viewContext
-          
-          let entity =
-            NSEntityDescription.entity(forEntityName: "Number",
-                                       in: managedContext)!
-          
-          let numb = NSManagedObject(entity: entity,
-                                       insertInto: managedContext)
-          
-        numb.setValue(numberField.text, forKeyPath: "num")
-          
           do {
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let mancon = appDelegate.persistentContainer.viewContext
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Number")
+            fetchRequest.returnsObjectsAsFaults = false
+
+            do
+            {
+                let results = try mancon.fetch(fetchRequest)
+                for managedObject in results
+                {
+                    let managedObjectData:NSManagedObject = managedObject as! NSManagedObject
+                    mancon.delete(managedObjectData)
+                }
+            } catch let error as NSError {
+                print("Detele all data in \("Number") error : \(error) \(error.userInfo)")
+            }
+            guard let appDelegate =
+                UIApplication.shared.delegate as? AppDelegate else {
+                return
+              }
+            let managedContext =
+                appDelegate.persistentContainer.viewContext
+              
+              let entity =
+                NSEntityDescription.entity(forEntityName: "Number",
+                                           in: managedContext)!
+              
+              let numb = NSManagedObject(entity: entity,
+                                           insertInto: managedContext)
+              
+            numb.setValue(numberField.text, forKeyPath: "num")
             try managedContext.save()
           } catch let error as NSError {
             print("Could not save. \(error), \(error.userInfo)")
@@ -63,10 +81,8 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
           
           do {
             let numb = try managedContext.fetch(fetchRequest)
-            if numb.count == 1{
+            if numb.count == 1 {
                 numberField.text = (numb[0].value(forKey: "num") as! String)
-            } else {
-                print(numb.count)
             }
           } catch let error as NSError {
             print("Could not fetch. \(error), \(error.userInfo)")
@@ -95,15 +111,16 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
             //Start the speech recognition service. It won't start listning until I start the microphone
             recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { result, error in
                 var isFinal = false
-                
+                if result != nil{
                 if let result = result {
                     isFinal = result.isFinal
                     //Check if the user said "help"
-                    if result.bestTranscription.formattedString.lowercased().contains("help") {
+                    if result.bestTranscription.formattedString.lowercased().contains("help") && self.called == false {
                         //If the user said "help", this code will run
+                        self.called = true //Stop the ask function from running
                         
                         //Run the call function
-                        self.call(Void())
+                        self.call(ViewController())
                     }
                 }
                 
@@ -114,8 +131,11 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
 
                     self.recognitionRequest = nil
                     self.recognitionTask = nil
-
+                    //Reload the audio engine
+                    self.reload()
+                    
                 }
+            }
             }
 
             
@@ -152,12 +172,128 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
             present(alert, animated: true)
         }
     }
+    func reload() {
+        //If the audio player crashes, this will reload it
+        
+        self.audioEngine.stop()
+        recognitionRequest?.endAudio()
+
+        self.recognitionRequest = nil
+        self.recognitionTask = nil
+        
+        SFSpeechRecognizer.requestAuthorization({_ in})
+        do{
+            //Prepare the microphone
+            recognitionTask?.cancel()
+            self.recognitionTask = nil
+            speechRecognizer.delegate = self
+            let audioSession = AVAudioSession.sharedInstance()
+            try audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+            let inputNode = audioEngine.inputNode
+            //Prepare a recognition request, which allows me to pr
+            recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+            guard let recognitionRequest = recognitionRequest else { fatalError("Unable to create a SFSpeechAudioBufferRecognitionRequest object") }
+            recognitionRequest.shouldReportPartialResults = true
+            
+            //Start the speech recognition service. It won't start listning until I start the microphone
+            recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { result, error in
+                var isFinal = false
+                if result != nil{
+                if let result = result {
+                    isFinal = result.isFinal
+                    //Check if the user said "help"
+                    if result.bestTranscription.formattedString.lowercased().contains("help") && self.called == false {
+                        //If the user said "help", this code will run
+                        self.called = true //Stop the ask function from running
+                        
+                        //Run the call function
+                        self.call(ViewController())
+                    }
+                }
+                
+                if error != nil || isFinal {
+                    // Stop recognizing speech if there is a problem.
+                    self.audioEngine.stop()
+                    inputNode.removeTap(onBus: 0)
+
+                    self.recognitionRequest = nil
+                    self.recognitionTask = nil
+                    //Reload the audio engine
+                    self.reload()
+                    
+                }
+            }
+            }
+
+            
+            //Create a tap and start the audio engine
+            let recordingFormat = inputNode.outputFormat(forBus: 0)
+            inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+                self.recognitionRequest?.append(buffer)
+            }
+            
+            audioEngine.prepare()
+            //Start the microphone
+            try audioEngine.start()
+            
+        }catch{
+            //If the app cannot access the microphone, this will run
+            
+            //Create an alert
+            let alert = UIAlertController(title: "Microphone error", message: "We were unable to access the microphone. To grant microphone access, tap settings", preferredStyle: .alert)
+            //Add a settings button to the alert
+            alert.addAction(UIAlertAction(title: "Settings", style: .default, handler: { (_) -> Void in
+                //If the user taps on the settings button, this code will run
+                guard let settingsUrl = URL(string: UIApplication.openSettingsURLString)/*Get the URL to open the settings app and go to this app's settings page*/ else {
+                            return
+                        }
+
+                        if UIApplication.shared.canOpenURL(settingsUrl) {
+                            //Open the settings app
+                            UIApplication.shared.open(settingsUrl)
+                        }
+            }))
+            //Add an ok button to the alert
+            alert.addAction(UIAlertAction(title: "Ok", style: .default))
+            //Show the alert
+            present(alert, animated: true)
+        }
+    }
+    func returnto(){
+        //When the app finishes the call, this code will run
+        
+        //Reload the audio driver
+        reload()
+    }
+    
     @IBAction func call(_ sender: Any) {
         //When the button is pressed or the word "help" is heard, this code will run
-        if let url = URL(string: "tel://\(String(numberField.text!))") {
-            //Call the phone number in the text box
-            UIApplication.shared.open(url)
+        
+        //Load the phone number
+        guard let appDelegate =
+          UIApplication.shared.delegate as? AppDelegate else {
+            return
         }
+        
+        let managedContext =
+          appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest =
+          NSFetchRequest<NSManagedObject>(entityName: "Number")
+        
+        do {
+          let numb = try managedContext.fetch(fetchRequest)
+          if numb.count == 1 {
+            if let url = URL(string: "tel://\(numb[0].value(forKey: "num") as! String)") {
+                //Call the phone number in the text box
+                UIApplication.shared.open(url)
+            }
+          }
+        } catch let error as NSError {
+          print("Could not fetch. \(error), \(error.userInfo)")
+        }
+        
     }
     //Hide the keyboard when the user taps out of it
     @objc func dismissKeyboard (_ sender: UITapGestureRecognizer) {
--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 This is my project for the Hospital Of The Future hackathon
 # What is this
 This is a tool that can be used for kids in hospitals to call for help with the push of a button, or by saying "help". If the button is pressed, or "help" is said, the app will call the number set in the app.
-
+# Instructions
+ - Before use, make sure the number in the phone number field is correct. <b>Only put the numbers.</b> Example: 1234567890
+- Say "help" or press the button to call for help.
+- Tap call to confirm
+- If you would like to use voice to confirm, do the following:
+    - Open settings
+    - Tap on "Accessibility"
+    - Tap on "Voice Control"
+    - Turn on "Voice Control"
+- Now, to use voice to confirm the call, say "tap call"
